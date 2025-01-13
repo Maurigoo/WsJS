333d71d3d1acdfbbed24844922060752270650d3
Jose Mauricio Herbas Becerra <jose.mauricio.herbas@bootcamp-upgrade.com>
2025-01-13 16:52:28 +0100

    primer paso

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const msj = "Aprendiendo Javascript"
+let number;
+
+console.log(msj.length);
+console.log(msj.toLowerCase().includes("Java".toLowerCase()));
+console.log(msj.toLowerCase().replace("javascript", "pyton"))
+console.log(msj.slice(0,11))
+console.log(msj.repeat(3))
+
+console.log(Math.ceil(Math.random()))
